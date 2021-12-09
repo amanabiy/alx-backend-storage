dc53660c6930c0311d1104b1b87d2978893d4db0
amanabiy <amanuelsisay.as@gmail.com>
2021-12-09 23:45:10 +0300

    Enum

---
+++ 0x00-MySQL_Advanced/0-uniq_users copy.sql
@@ -0,0 +1,8 @@
+-- creates a table users
+-- attributes Id, email, name, and country
+CREATE TABLE IF NOT EXISTS `users`(
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    `country` ENUM('US', 'CO', 'TN') NOT NULL
+)
