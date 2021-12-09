f61787318909655617ab1a638e51ccacdd11bc6f
amanabiy <amanuelsisay.as@gmail.com>
2021-12-10 00:44:10 +0300

    order by lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- a SQL script that lists all bands with Glam rock
 -- as their main style, ranked by their longevity
-SELECT DISTINCT `band_name`, IFNULL(`split`, 2021) - `formed` as `lifespan`
+SELECT DISTINCT `band_name`, IFNULL(`split`, 2020) - `formed` as `lifespan`
 FROM `metal_bands` WHERE  style Like '%Glam rock%' ORDER BY `lifespan` DESC
