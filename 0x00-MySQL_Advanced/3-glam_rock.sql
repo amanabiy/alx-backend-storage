f0430ca2b22a65813e221adfd8c8ff6e50db5a3a
amanabiy <amanuelsisay.as@gmail.com>
2021-12-10 00:33:04 +0300

    fileter with where

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- a SQL script that lists all bands with Glam rock
+-- as their main style, ranked by their longevity
+SELECT `band_name`, `lifespan` FROM `metal_bands`
+WHERE lifespan = split - formed
+ORDER BY `nb_fans` DESC
