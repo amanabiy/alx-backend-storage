0d481d1a7f4a167cc232b508d23a78db8b26098e
amanabiy <amanuelsisay.as@gmail.com>
2021-12-10 11:47:20 +0300

    added a delimeter

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,6 @@
 -- a SQL script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id int)
 BEGIN
     DECLARE average float;
@@ -7,3 +8,4 @@ BEGIN
         WHERE corrections.user_id = user_id;
     UPDATE users SET average_score = average WHERE id = user_id; 
 END
+$$
