d3c543c212e4e985c1182db23e4680a08e20e24b
amanabiy <amanuelsisay.as@gmail.com>
2021-12-10 11:45:43 +0300

    updates user average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,9 @@
+-- a SQL script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student.
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id int)
+BEGIN
+    DECLARE average float;
+    SELECT AVG(score) INTO average FROM corrections
+        WHERE corrections.user_id = user_id;
+    UPDATE users SET average_score = average WHERE id = user_id; 
+END
