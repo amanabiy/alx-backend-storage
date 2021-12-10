58ab50ebf3758f05af1405bca2f804423daaee3a
amanabiy <amanuelsisay.as@gmail.com>
2021-12-10 11:20:39 +0300

    use if

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,16 +2,24 @@
 -- adds a correction for a student
 CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
 BEGIN
-    IF NOT EXISTS (SELECT * FROM projects
-                    WHERE name = project_name)
-    BEGIN
-        INSERT INTO projects (name) VALUES (project_name)
-    END If
+    DECLARE p_name VARCHAR(255);
+    SELECT id INTO p_name FROM projects WHERE name = project_name;
+    IF p_name IS NULL
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SELECT id INTO p_name FROM projects WHERE name = project_name;
+    END IF;
+
+    -- IF NOT EXISTS (SELECT * FROM projects
+    --                 WHERE name = project_name)
+    -- BEGIN
+    --     INSERT INTO projects (name) VALUES (project_name)
+    -- END If
                     
     INSERT INTO corrections (user_id, project_id, score)
         VALUES (
             user_id,
-            (SELECT id FROM projects WHERE name = project_name),
+            p_name,
             score
         );
 END
