0f9c1d7196bbc312f31bbb58187221e8c03702ea
amanabiy <amanuelsisay.as@gmail.com>
2021-12-10 11:24:34 +0300

    float for score

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,45 +1,30 @@
 -- add Bonus procedure
 -- adds a correction for a student
-DELIMITER $ $ CREATE PROCEDURE AddBonus (
-    IN user_id int,
-    IN project_name varchar(255),
-    IN score int
-) BEGIN DECLARE p_name VARCHAR(255);
+DELIMITER $$
+CREATE PROCEDURE AddBonus (
+  IN user_id int,
+  IN project_name varchar(255),
+  IN score float)
+BEGIN
+    DECLARE p_name VARCHAR(255);
+    SELECT id INTO p_name FROM projects WHERE name = project_name;
+    IF p_name IS NULL
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SELECT id INTO p_name FROM projects WHERE name = project_name;
+    END IF;
 
-SELECT
-    id INTO p_name
-FROM
-    projects
-WHERE
-    name = project_name;
-
-IF p_name IS NULL THEN
-INSERT INTO
-    projects (name)
-VALUES
-    (project_name);
-
-SELECT
-    id INTO p_name
-FROM
-    projects
-WHERE
-    name = project_name;
-
-END IF;
-
--- IF NOT EXISTS (SELECT * FROM projects
---                 WHERE name = project_name)
--- BEGIN
---     INSERT INTO projects (name) VALUES (project_name)
--- END If
-INSERT INTO
-    corrections (user_id, project_id, score)
-VALUES
-    (
-        user_id,
-        p_name,
-        score
-    );
-
-END $ $
+    -- IF NOT EXISTS (SELECT * FROM projects
+    --                 WHERE name = project_name)
+    -- BEGIN
+    --     INSERT INTO projects (name) VALUES (project_name)
+    -- END If
+                    
+    INSERT INTO corrections (user_id, project_id, score)
+        VALUES (
+            user_id,
+            p_name,
+            score
+        );
+END
+$$
