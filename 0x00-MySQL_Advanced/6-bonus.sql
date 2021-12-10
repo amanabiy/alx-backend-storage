8a637099973050000cf572c1e6a71217557e840c
amanabiy <amanuelsisay.as@gmail.com>
2021-12-10 11:10:24 +0300

    correct syntax

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,12 +2,12 @@
 -- adds a correction for a student
 CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
 BEGIN
-    IF NOT EXISTS (
-        SELECT * FROM projects
-        WHERE name = project_name)
+    IF NOT EXISTS (SELECT * FROM projects
+                    WHERE name = project_name)
         BEGIN
             INSERT INTO projects (name) VALUES (project_name)
         END
+                    
     INSERT INTO corrections (user_id, project_id, score)
         VALUES (
             user_id,
