c6c35670dcb267f6de9854015304fdb0776f2a32
amanabiy <amanuelsisay.as@gmail.com>
2021-12-10 22:57:40 +0300

    syntax correction

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,6 +1,6 @@
 -- a SQL script that creates a function SafeDiv that divides (and returns)
 -- the first by the second number or returns 0 if the second number is equal to 0.
-CREATE FUNCTION SafeDiv [ a INT, b int]
+CREATE FUNCTION SafeDiv (a INT, b int)
 RETURNS float
 BEGIN
     IF b = 0
