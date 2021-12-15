25fa650ef2b283cb6c28a400d9535e4f67b12b7a
amanabiy <amanuelsisay.as@gmail.com>
2021-12-15 23:04:26 +0300

    added get_int and get_str functions

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,9 +4,20 @@
  generate a random key (e.g. using uuid),
  store the input data in Redis using the random key and return the key.
 """
-from typing import Union
+from typing import Callable, Optional, Union
 import redis
+import sys
 from uuid import uuid4
+
+
+def get_str(self, data: bytes) -> str:
+    """ Bytes to string """
+    return data.decode('utf-8')
+
+
+def get_int(self, data: bytes) -> int:
+    """ Bytes to integer """
+    return int.from_bytes(data, sys.byteorder)
 
 
 class Cache:
@@ -14,8 +25,8 @@ class Cache:
 
     def __init__(self) -> None:
         """instantiate the class"""
-        _redis = redis.Redis()
-        _redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(data: Union[int, str, bytes, float]) -> str:
         """
@@ -24,3 +35,9 @@ class Cache:
         key: str = str(uuid4())
         redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            func: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ Gets  """
+        res = self._redis.get(key)
+        return func(res) if func else res
